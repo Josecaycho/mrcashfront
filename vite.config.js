ee9bd8972b8c7f194a691d471cd14dcf2a26e73e
Jose Caycho <jose30caycho@gmail.com>
2024-04-16 00:36:13 -0500

    asdsa

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/scss/global.scss";`,
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      vuetify({ autoImport: true }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/global.scss";`,
+        },
       },
     },
-  },
-  server: {
-    cors: true,
-    proxy: {
-      "/api": {
-        ws: true,
-        // target: "http://54.166.130.134:3002",
-        target: "http://localhost:4000",
-        secure: false,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    server: {
+      cors: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_APP_URL_API,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        }
       }
     }
   }
